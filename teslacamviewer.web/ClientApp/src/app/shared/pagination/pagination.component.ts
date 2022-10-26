0b23004987521415f04fe4483d4a7e1e7225badf
Rodolfo Gongora <rodolfogongora@gmail.com>
2022-10-25 23:56:24 -0500

    pretty cool pagination lol

--- teslacamviewer.web/ClientApp/src/app/shared/pagination/pagination.component.ts
+++ teslacamviewer.web/ClientApp/src/app/shared/pagination/pagination.component.ts
@@ -20,4 +20,29 @@ export class PaginationComponent implements OnInit {
     this.pageChanged$.next(page);
   }
 
+  getPages() {
+    const pages = [];
+    const lowerBound = this.clamp(this.currentPage - 2, 1, this.totalPages);
+    const upperBound = this.clamp(this.currentPage + 2, 1, this.totalPages);
+    console.log(lowerBound, upperBound); // 1 5
+    for(let i = lowerBound; i <= upperBound; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
+  
+  morePagesToTheRight(): boolean {
+    return this.currentPage + 2 < this.totalPages;
+  }
+
+  morePagesToTheLeft(): boolean {
+    return this.currentPage - 2 > 1;
+  }
+
+  movePagination(numberOfPages: number) {
+    this.changePage(this.clamp(this.currentPage + numberOfPages, 1, this.totalPages)); // 3 5
+  }
+
+  clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max);
+
 }
